27fae4b996e9dccf49b610e14d9092ba13fe7ad6
kilon <thekilon@yahoo.co.uk>
2016-10-27 12:32:43 +0300

    refine init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,6 @@
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(setq cabbage-repository (expand-file-name "/Users/kilon/.emacs.d/cabbage/"))
+(setq cabbage-repository (expand-file-name "~/.emacs.d/cabbage/"))
 (load (concat cabbage-repository "cabbage"))
 (load "~/.emacs.d/pillar-latex2pillar.el")
